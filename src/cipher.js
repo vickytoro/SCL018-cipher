af77e99242869d90048d1781e20748687a8a1f69
Fritza02 <fritza.v@hotmail.com>
2021-09-06 22:30:58 -0300

    Proyecto con Javascript terminado, mañana son las Demos

--- src/cipher.js
+++ src/cipher.js
@@ -1,29 +1,43 @@
 const cipher = {
 
     codificar: function(mensaje1,desplazamiento) {
+    //console.log(mensaje1,desplazamiento);
+    let mensEncod = "";
 
-    let mensCodif = "";
-
-    for(i=0; i<mensaje1.lenght; i++) {
-
-        let numSecret = (mensaje1.charCodeAt(i) - 65 + desplazamiento)%26 +65;
-        let mensScret = String.fromCharCode(numSecret);
-        mensCodif = numSecret + mensScret;
-
+    for(let i=0; i<mensaje1.length; i++) {
+      
+        let numAcsii = mensaje1.charCodeAt(i); //letras transformadas a código Ascii
+        
+        //console.log("numAcsii: " + numAcsii);
+        //console.log(desplazamiento);
+        let newPosition = (numAcsii - 65 + desplazamiento)%26 + 65; //posición de las nuevas letras ya codificadas
+        //console.log("nueva posición: "+ nuevPosicion);
+        mensEncod += String.fromCharCode(newPosition);
+        //console.log(mensCodif);
         }
-
-      console.log(mensCodif);
-      return mensCodif;
-
-    },
+      return mensEncod;
+      },
   
     decodificar: function(mensaje1,desplazamiento){
 
-      console.log("función decodificar");
+        let mensDecod = "";
+        
+      for(let i=0; i<mensaje1.length; i++) {
+          
+        let numAcsii = mensaje1.charCodeAt(i); //letras transformadas a código Ascii
+        //console.log("numAcsii: " + numAcsii);
+        //console.log(-desplazamiento + 0);
+        let parenthesis = numAcsii-65-desplazamiento;
 
-      return "PROBANDO2";
-
-    }
+        while(parenthesis<0){//while que obliga a que el valor del () sea siempre positivo
+        parenthesis=parenthesis+26;
+            }
+        let newPosition = parenthesis%26 + 65; //posición de las nuevas letras ya codificadas
+        //console.log("nueva posición: "+ newPosition);
+        mensDecod += String.fromCharCode(newPosition);
+        //console.log(mensCodif);
+          }
+      return mensDecod;
+      }
 };
-
 export default cipher;
